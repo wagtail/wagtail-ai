9db4db3da623d8aeea94a4d7384aa674631df028
Ben Morse <ben.morse@torchbox.com>
2023-12-08 16:16:57 +0000

    Cleanup tests

--- tests/test_prompts.py
+++ tests/test_prompts.py
@@ -4,10 +4,8 @@ from wagtail_ai.models import Prompt
 from wagtail_ai.prompts import DEFAULT_PROMPTS
 from wagtail_ai.wagtail_hooks import get_prompts
 
-# Apply pytest.mark.django_db to all tests in this file
-pytestmark = pytest.mark.django_db
 
-
+@pytest.mark.django_db
 def test_prompt_model(setup_prompt_object, test_prompt_values):
     assert setup_prompt_object.is_default is False
     assert str(setup_prompt_object) == test_prompt_values["label"]
@@ -17,41 +15,53 @@ def test_prompt_model(setup_prompt_object, test_prompt_values):
     assert setup_prompt_object.method == test_prompt_values["method"]
 
 
+@pytest.mark.django_db
 def test_get_prompts_returns_default_prompts():
     prompts = get_prompts()
-    default_prompts_queryset = Prompt.objects.filter(
-        default_prompt_id__isnull=False
-    ).all()
 
-    assert len(default_prompts_queryset) == len(DEFAULT_PROMPTS)
-    assert len(prompts) == len(DEFAULT_PROMPTS)
-    assert prompts[0]["label"] == DEFAULT_PROMPTS[0]["label"]
-    assert prompts[0]["prompt"] == DEFAULT_PROMPTS[0]["prompt"]
+    assert set((p["label"], p["prompt"]) for p in prompts) == set(  # noqa: C401
+        (p["label"], p["prompt"]) for p in DEFAULT_PROMPTS
+    )
 
 
+def find_prompt_by_label(prompts, label):
+    return next((prompt for prompt in prompts if prompt["label"] == label), None)
+
+
+@pytest.mark.django_db
 def test_editing_default_prompts():
+    # Get a default prompt and check it's returned by get_prompts
     prompts = get_prompts()
-    assert prompts[0]["label"] == DEFAULT_PROMPTS[0]["label"]
-    assert prompts[0]["prompt"] == DEFAULT_PROMPTS[0]["prompt"]
-
     default_prompt = Prompt.objects.get(
         default_prompt_id=DEFAULT_PROMPTS[0]["default_prompt_id"]
     )
+    prompt_from_get_prompts = find_prompt_by_label(prompts, default_prompt.label)
+    assert prompt_from_get_prompts is not None
 
+    # Check the prompt value is the same as the default prompt value
+    assert default_prompt.prompt is None
+    assert prompt_from_get_prompts["prompt"] == default_prompt.prompt_value
+
+    # Add a prompt value to the default prompt
     default_prompt.prompt = "New Prompt Text"
     default_prompt.save()
     assert default_prompt.prompt_value == "New Prompt Text"
 
+    # Check the updated prompt value is returned by get_prompts
     prompts = get_prompts()
-    assert prompts[0]["prompt"] == "New Prompt Text"
-    assert prompts[0]["prompt"] != DEFAULT_PROMPTS[0]["prompt"]
+    prompt_from_get_prompts = find_prompt_by_label(prompts, default_prompt.label)
+    assert prompt_from_get_prompts is not None
+    assert prompt_from_get_prompts["prompt"] == default_prompt.prompt_value
+    assert prompt_from_get_prompts["prompt"] == default_prompt.prompt
 
 
+@pytest.mark.django_db
 def test_get_prompts_returns_new_prompts_and_default_prompts(setup_prompt_object):
+    prompt_object = setup_prompt_object
     prompts = get_prompts()
-    assert (
-        len(prompts) == len(DEFAULT_PROMPTS) + 1
-    )  # 1 Prompt from setup_prompt_object is added
+    assert set(p["label"] for p in prompts) == set(  # noqa: C401
+        [p["label"] for p in DEFAULT_PROMPTS] + [prompt_object.label]
+    )
 
     # Check setup_prompt_object is in get_prompts
     assert setup_prompt_object.label in [prompt["label"] for prompt in prompts]
@@ -63,15 +73,17 @@ def test_get_prompts_returns_new_prompts_and_default_prompts(setup_prompt_object
         assert prompt["prompt"] in [prompt["prompt"] for prompt in prompts]
 
 
+@pytest.mark.django_db
 def test_prompts_return_uuids_and_not_ids():
     prompts = get_prompts()
     assert prompts[0]["uuid"] is not None
     assert "id" not in prompts[0]
 
 
+@pytest.mark.django_db
 def test_prompts_can_not_save_and_invalid_method(test_prompt_values):
-    with pytest.raises(ValidationError):
-        prompt = Prompt.objects.create(
+    with pytest.raises(ValidationError, match="Value 'foo' is not a valid choice."):
+        prompt = Prompt(
             label=test_prompt_values["label"],
             description=test_prompt_values["description"],
             prompt=test_prompt_values["prompt"],
