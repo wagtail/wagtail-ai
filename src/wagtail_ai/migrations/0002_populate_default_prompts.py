909b1cc4e268e19b62434762632af29df79afd5e
Ben Morse <ben.morse@torchbox.com>
2023-12-08 15:14:59 +0000

    Update update_or_create to only update when prompt=None

--- src/wagtail_ai/migrations/0002_populate_default_prompts.py
+++ src/wagtail_ai/migrations/0002_populate_default_prompts.py
@@ -14,12 +14,12 @@ def set_default_ai_prompts(apps, schema_editor):
     for default_prompt in DEFAULT_PROMPTS:
         Prompt.objects.update_or_create(
             default_prompt_id=default_prompt["default_prompt_id"],
+            # The prompt field is left blank to allow users to override default prompt values and maintainers
+            # to manage default prompts that are None (not overridden) using the get_default_prompt_value method.
+            prompt=None,
             defaults={
                 "uuid": default_prompt["uuid"],
                 "label": default_prompt["label"],
-                # The prompt field is left blank to allow users to override default prompt values and maintainers
-                # to manage default prompts that are None (not overridden) using the get_default_prompt_value method.
-                "prompt": None,
                 "description": default_prompt.get("description", ""),
                 "method": default_prompt.get("method", None),
             },
