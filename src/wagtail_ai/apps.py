ded87e6bca57e25477b00c8502ab9221681b0cc6
Stefan Hammer <stefan@hammerworxx.com>
2024-03-05 17:14:42 +0100

    Add default_auto_field (#64)

--- src/wagtail_ai/apps.py
+++ src/wagtail_ai/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 
 
 class WagtailAiAppConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
     label = "wagtail_ai"
     name = "wagtail_ai"
     verbose_name = "Wagtail AI"
