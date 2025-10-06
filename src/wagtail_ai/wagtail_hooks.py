68d39cba3486479987236557fcbf0a0393b0b14a
Sage Abdullah <sage.abdullah@torchbox.com>
2025-10-06 17:23:26 +0100

    Append slash to url path

--- src/wagtail_ai/wagtail_hooks.py
+++ src/wagtail_ai/wagtail_hooks.py
@@ -43,7 +43,7 @@ def register_admin_urls():
             name="content_feedback",
         ),
         path(
-            "suggested-content",
+            "suggested-content/",
             SuggestedContentAgent.as_view(),
             name="suggested_content",
         ),
